05c99550bf1e4405cde8439a07afb3e46a1eb679
Thiago Barreto Clemente <thiagobcdev@gmail.com>
2023-02-19 22:51:15 -0300

    DOM events

---
+++ js/eventos.js
@@ -0,0 +1,28 @@
+// EVENTO ONCLICK
+
+function color(){
+    let p = document.querySelector(".p0");
+    p.style.color = "blue";
+}
+
+function color1(){
+    let p = document.querySelector(".p1");
+    p.style.color = "green";
+    p.style.fontWeight = 700;
+}
+
+function color2(){
+    let p = document.querySelector(".p2");
+    p.style.color = "#ffcccc";
+}
+
+function color3(){
+    let p = document.querySelector(".p3");
+    // p.style.color = "##fff2cc";
+    p.style.backgroundColor = "#fff2cc";
+}
+
+function color4(){
+    let p = document.querySelector(".p4");
+    p.style.backgroundColor = "#ccd9ff";
+}
